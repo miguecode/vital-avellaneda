9fa7e758af3197f8b0d1353a07892f291d71553e
Leumig <junmigue7@gmail.com>
2025-07-17 05:14:40 -0300

    Estilos generales y retoques visuales más pulidos

--- src/app/shared/components/header-auth/header-auth.component.ts
+++ src/app/shared/components/header-auth/header-auth.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { SvgIconComponent } from "../../icons/svg-icon.component";
 
 @Component({
   selector: 'app-header-auth',
-  imports: [RouterLink],
+  imports: [RouterLink, SvgIconComponent],
   templateUrl: './header-auth.component.html',
   styleUrl: './header-auth.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/shared/components/header-dashboard/header-dashboard.component.ts
+++ src/app/shared/components/header-dashboard/header-dashboard.component.ts
@@ -3,10 +3,11 @@ import { RouterLink } from '@angular/router';
 import { AuthFacade } from '../../../features/auth/auth.facade';
 import { UserSubmenuComponent } from '../user-submenu/user-submenu.component';
 import { UserRoles as R } from '../../../core/enums';
+import { SvgIconComponent } from "../../icons/svg-icon.component";
 
 @Component({
   selector: 'app-header-dashboard',
-  imports: [RouterLink, UserSubmenuComponent],
+  imports: [RouterLink, UserSubmenuComponent, SvgIconComponent],
   templateUrl: './header-dashboard.component.html',
   styleUrl: './header-dashboard.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,4 +20,11 @@ export class HeaderDashboardComponent {
   this.user()?.rol === R.PATIENT
     ? 'Portal para Pacientes'
     : 'Portal para Especialistas';
+
+  get userRoleIcon(): 'user' | 'patient' | 'specialist' {
+    const rol = this.user()?.rol;
+    if (rol === R.PATIENT) return 'patient';
+    if (rol === R.SPECIALIST) return 'specialist';
+    return 'user';
+  }
 }
